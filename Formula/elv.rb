2d727364412b036eb89aa8e49544f23ff2e736c1
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-01-18 23:26:00 +0100

    Update elv.rb

--- Formula/elv.rb
+++ Formula/elv.rb
@@ -1,7 +1,7 @@
 class Elv < Formula
   desc "Little CLI helper for Advent of Code. 🎄"
   homepage "https://github.com/kpagacz/elv"
-  url "https://github.com/kpagacz/elv/archive/refs/tags/elv-0.12.1.tar.gz"
+  url "https://github.com/kpagacz/elv/archive/refs/tags/v0.12.1.tar.gz"
   sha256 "281c923dd15e51299d97b2bc2292e1a116ffcad72b66451079d38d3460fbef6e"
 
   depends_on "rust" => :build
