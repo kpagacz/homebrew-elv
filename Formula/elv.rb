7d7b59f616c9b1701e8b40513dbdca3d4908213e
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-07-18 00:07:46 +0200

    removed bottle, updated style

--- Formula/elv.rb
+++ Formula/elv.rb
@@ -1,5 +1,5 @@
 class Elv < Formula
-  desc "Little CLI helper for Advent of Code. 🎄"
+  desc "Little CLI helper for Advent of Code"
   homepage "https://github.com/kpagacz/elv"
   url "https://github.com/kpagacz/elv/archive/refs/tags/v0.12.3.tar.gz"
   sha256 "38623707c39c0005698ce2d933d027a12d1f3a67f53b9167b0bbadb014d4b06d"
