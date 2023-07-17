791962be3d800a17d0bb585dcca9ed0546316bbb
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-07-18 00:02:30 +0200

    new version

--- Formula/elv.rb
+++ Formula/elv.rb
@@ -1,10 +1,12 @@
 class Elv < Formula
   desc "Little CLI helper for Advent of Code. 🎄"
   homepage "https://github.com/kpagacz/elv"
-  url "https://github.com/kpagacz/elv/archive/refs/tags/v0.12.1.tar.gz"
-  sha256 "281c923dd15e51299d97b2bc2292e1a116ffcad72b66451079d38d3460fbef6e"
+  url "https://github.com/kpagacz/elv/archive/refs/tags/v0.12.3.tar.gz"
+  sha256 "38623707c39c0005698ce2d933d027a12d1f3a67f53b9167b0bbadb014d4b06d"
+  license ""
 
   depends_on "rust" => :build
+
   def install
     system "cargo", "install", *std_cargo_args
     bin.install "target/release/elv"
@@ -12,11 +14,5 @@ class Elv < Formula
 
   test do
     system "elv", "--version"
-  end
-
-  bottle do
-    root_url "https://github.com/kpagacz/homebrew-elv/raw/main/Bottles/v0.12.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, ventura: "7bb5481a01fe3681fac1f0a3c38faafc743e887d6268bf4a15356619d684cadc"
   end
 end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Kpagacz Elv
+
+## How do I install these formulae?
+
+`brew install kpagacz/elv/<formula>`
+
+Or `brew tap kpagacz/elv` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
