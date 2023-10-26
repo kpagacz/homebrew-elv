7ce1349a6fd33ea154515700cc327148d0e6eaa7
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-10-26 22:53:22 +0200

    v0.13.1

--- Formula/elv.rb
+++ Formula/elv.rb
@@ -1,8 +1,8 @@
 class Elv < Formula
   desc "Little CLI helper for Advent of Code"
   homepage "https://github.com/kpagacz/elv"
-  url "https://github.com/kpagacz/elv/archive/refs/tags/v0.12.3.tar.gz"
-  sha256 "38623707c39c0005698ce2d933d027a12d1f3a67f53b9167b0bbadb014d4b06d"
+  url "https://github.com/kpagacz/elv/archive/refs/tags/v0.13.1.tar.gz"
+  sha256 "60d1a7c6a28c32cec2fee0e18e33e0810b0523fe4bd21ca9b87a496a9f42b99b"
   license ""
 
   depends_on "rust" => :build
